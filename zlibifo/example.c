c6f95dafcba710233a63a87346ed0661d2ab0d6e
Jonathan Campbell <jonathan@castus.tv>
2025-11-06 19:45:35 -0800

    zlibifo put crc32 back

--- zlibifo/example.c
+++ zlibifo/example.c
@@ -32,7 +32,9 @@ struct chksumtest_t {
 	char*		txt;
 	unsigned short	len;
 	uint32_t	adler32_init;
+	uint32_t	crc32_init;
 	uint32_t	adler32_final;
+	uint32_t	crc32_final;
 };
 
 #if defined(_WINDOWS) || defined(WIN32) || defined(WIN386)
@@ -84,12 +86,13 @@ const char hello[] = "hello, hello!";
 int  main               OF((int argc, char *argv[]));
 
 const struct chksumtest_t chktest[] = {
-{"",0,0x00000001,0x00000001},
-{"test",4,0x00000001,0x045d01c1},
-{"testing123",10,0x00000001,0x16450395},
-{"iuqwpoiuirhjke-09328-0q8ewaf987ad098f7sad9f8puahpwrouae8n7fg37r098w7r98we7rf098we7r098we7~0fd~~sdf~~sf~sa~d~~sad~~sad~7wuiqoueyuwidyqwuioryqwouifdqowuiryqw8e79qw8e",163,0x00000001,0xa9423d0f},
+{"",0,0x00000001,0x00000000,0x00000001,0x00000000},
+{"test",4,0x00000001,0x00000000,0x045d01c1,0xd87f7e0c},
+{"testing123",10,0x00000001,0x00000000,0x16450395,0xe82509f0},
+{"iuqwpoiuirhjke-09328-0q8ewaf987ad098f7sad9f8puahpwrouae8n7fg37r098w7r98we7rf098we7r098we7~0fd~~sdf~~sf~sa~d~~sad~~sad~7wuiqoueyuwidyqwuioryqwouifdqowuiryqw8e79qw8e",163,0x00000001,0x00000000,0xa9423d0f,0x4b87f53e},
 {NULL}
 };
+
 
 static const unsigned char inft1_zlib[1246] = {
 /*0000*/ 0x78,0xDA,0x8D,0x56,0xDF,0x4F,0xE4,0x36,0x10,0x7E,0xE7,0xAF,0x98,0x5E,0x5F,0x80,
@@ -407,23 +410,33 @@ void test_inflate_data(uncompr, uncomprLen)
 
 void test_crcs() {
     unsigned int i;
-    uint32_t a;
+    uint32_t c,a;
 
     for (i=0;chktest[i].txt;i++) {
         a = adler32(0,NULL,0);
+        c = crc32(0,NULL,0);
 
         if (a != chktest[i].adler32_init) {
             fprintf(stderr,"adler32 check fail i=%u msg=%s expect=0x%08lx got=0x%08lx\n",
                 i,(const unsigned char*)chktest[i].txt,(unsigned long)chktest[i].adler32_init,(unsigned long)a);
         }
+        if (c != chktest[i].crc32_init) {
+            fprintf(stderr,"crc32 check fail i=%u msg=%s expect=0x%08lx got=0x%08lx\n",
+                i,(const unsigned char*)chktest[i].txt,(unsigned long)chktest[i].crc32_init,(unsigned long)c);
+        }
 
         if (chktest[i].len) {
             a = adler32(a,(const unsigned char*)chktest[i].txt,chktest[i].len);
+            c = crc32(c,(const unsigned char*)chktest[i].txt,chktest[i].len);
         }
 
         if (a != chktest[i].adler32_final) {
             fprintf(stderr,"adler32 check2 fail i=%u msg=%s expect=0x%08lx got=0x%08lx\n",
                 i,(const unsigned char*)chktest[i].txt,(unsigned long)chktest[i].adler32_final,(unsigned long)a);
+        }
+        if (c != chktest[i].crc32_final) {
+            fprintf(stderr,"crc32 check2 fail i=%u msg=%s expect=0x%08lx got=0x%08lx\n",
+                i,(const unsigned char*)chktest[i].txt,(unsigned long)chktest[i].crc32_final,(unsigned long)c);
         }
     }
 }
